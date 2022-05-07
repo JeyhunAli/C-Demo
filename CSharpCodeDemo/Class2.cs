1b7933c4daafc59ea56920ccca0e1a0f174ac3ac
Jeyhun <jeyhun_aliyev@partech.com>
2022-05-07 16:26:57 -0400

    starting C#demo

---
+++ CSharpCodeDemo/Class2.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpCodeDemo
+{
+    internal class Class2
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("hey class 2");
+        }
+    }
+}
