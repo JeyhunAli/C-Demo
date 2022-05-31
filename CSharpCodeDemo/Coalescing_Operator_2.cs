e6b4dfc18ba2f8fe7fefedf9e3dcbdce7a3d2b5a
Jeyhun <jeyhun_aliyev@partech.com>
2022-05-31 13:42:39 -0400

    new classes added

---
+++ CSharpCodeDemo/Coalescing_Operator_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace CSharpCodeDemo
+{
+
+    public class Person2
+    {
+        public string? Name { get; set; }
+    }
+
+    class Coalescing_Operator_2
+    {
+
+        static void Main(string[] args)
+        {
+
+            Person2? person = new Person2();
+            
+                person.Name = null;
+          //    person.Name = "Jey";
+
+
+            Console.WriteLine(person?.Name ?? "Person is null");
+
+        }
+    }
+}
+            
