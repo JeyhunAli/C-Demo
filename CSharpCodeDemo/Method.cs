bf53e88dde5b16deff16de8c5bbcc7494fb496ec
Jeyhun <jeyhun_aliyev@partech.com>
2022-06-02 10:10:28 -0400

    new classes added

---
+++ CSharpCodeDemo/Method.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCodeDemo
+{
+
+    // sim tab twice for main method
+    public class Method
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Add(29, 98));
+            
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ CSharpCodeDemo/ParsingStringToInteger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCodeDemo
+{
+    public class ParsingStringToInteger
+    {
+        public static void Main(string[] args)
+        {
+            //parsing means create int out of string
+            string s = "10";
+            string s2 = "20";
+
+            string ss = "10";
+            string sd = "20";
+
+            int num1 = Convert.ToInt32(s);
+            int num2 = Convert.ToInt32(s2);
+
+            int num3 = Int32.Parse(ss);
+            int num4 = Int32.Parse(sd);
+
+            int result = num1 + num2; 
+            Console.WriteLine("result is " +result);
+
+            int result2 = num3 - num4;
+            Console.WriteLine("result2 is " + result2);
+
+
+
+            string stringForFloat = "0.85"; // datatype should be float
+            string stringForInt = "12345"; // datatype should be int
+
+            float mynewfloat = float.Parse(stringForFloat);
+            int mynewInt = Convert.ToInt32(stringForInt);
+            Console.WriteLine("mynewfloat is " + mynewfloat + " mynewInt is " + mynewInt);
+        }
+    }
+}
