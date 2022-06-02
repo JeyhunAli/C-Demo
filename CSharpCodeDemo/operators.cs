152e10d6e283f0d739ce4f3cf3b41d8b1bd6dee8
Jeyhun <jeyhun_aliyev@partech.com>
2022-06-02 10:34:40 -0400

    new classes added

---
+++ CSharpCodeDemo/operators.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCodeDemo
+{
+    internal class operators
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+            //So the first one of them want to show you is going to store the value of minus number one in number three.
+            //So that's going to do is it will multiply number one with minus one.
+            //If we run that, so CW console, right, line, double tap, and in here I'm going to write something like it.
+            //Number three is and here I use curly brackets zero four.
+            //That will do is it will simply replace this whole thing here with the first variable that comes after
+            // unary operators
+            num3 = -num1;
+            Console.WriteLine("num3 is {0}", num3);
+
+            bool isSunny = true;
+            Console.WriteLine("is it sunny? {0}", !isSunny);
+
+            // increment operators
+            int num = 0;
+            num++;
+            Console.WriteLine("num is {0}", num);
+            Console.WriteLine("num is {0}", num++);
+            // pre increment
+            Console.WriteLine("num is {0}", ++num);
+
+            // decrement opertor
+            num--;
+            Console.WriteLine("num is {0}", num);
+            Console.WriteLine("num is {0}", num--);
+            // pre decrement
+            Console.WriteLine("num is {0}", --num);
+
+            int result;
+
+            result = num1 + num2;
+            Console.WriteLine("result of num1 + num2 is {0}", result);
+            result = num1 - num2;
+            Console.WriteLine("result of num1 - num2 is {0}", result);
+            result = num1 / num2;
+            Console.WriteLine("result of num1 / num2 is {0}", result);
+            result = num1 * num2;
+            Console.WriteLine("result of num1 * num2 is {0}", result);
+            result = num1 % num2;
+            Console.WriteLine("result of num1 % num2 is {0}", result);
+
+            // relational and type operators
+            bool isLower;
+            isLower = num1 < num2;
+            Console.WriteLine("result of num1 < num2 is {0}", isLower);
+
+            // equality operator
+            bool isEqual;
+            isEqual = num1 == num2;
+            Console.WriteLine("result of num1 == num2 is {0}", isEqual);
+
+            isEqual = num1 != num2;
+            Console.WriteLine("result of num1 != num2 is {0}", isEqual);
+
+            // conditional operators
+            bool isLowerAndSunny;
+            // condition1 AND condition2
+            isLowerAndSunny = isLower && isSunny;
+            Console.WriteLine("result of isLower && isSunny is {0}", isLowerAndSunny);
+
+            // condition1 OR condition2
+            isLowerAndSunny = isLower || isSunny;
+            Console.WriteLine("result of isLower || isSunny is {0}", isLowerAndSunny);
+            Console.ReadKey();
+        }
+    }
+}
