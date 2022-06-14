f03386276e6dbe3fcf034d27e90c3d2561766b28
Jeyhun <jeyhun_aliyev@partech.com>
2022-06-14 09:50:25 -0400

    new class and modification

---
+++ CSharpCodeDemo/PropertiesOfCSharp/ClassProperties1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCodeDemo.PropertiesOfCSharp
+{
+    public class ClassProperties1
+    {
+        static void Main(string[] args)
+        {
+            ClassProperties2 classs = new ClassProperties2(25, 25);
+         
+            Console.WriteLine("result " + classs.numbers + classs.numbers2);
+
+
+        }
+
+    }
+
+
+    class ClassProperties2
+    {
+        public int numbers { get; set; }
+        public int numbers2 { get; set; }
+
+        public ClassProperties2(int numbers, int numbers2)
+        {
+            this.numbers = numbers;
+            this.numbers2 = numbers2;
+
+        }
+    }
+}
