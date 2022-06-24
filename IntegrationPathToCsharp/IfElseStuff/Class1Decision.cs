8856f3bda0db63204c3bb6d7ccb3863987fb8588
Jeyhun <jeyhun_aliyev@partech.com>
2022-06-24 14:19:07 -0400

    new project all about c sharp related stuff

---
+++ IntegrationPathToCsharp/IfElseStuff/Class1Decision.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationPathToCsharp.IfElseStuff
+{
+    internal class Class1Decision
+    {
+        static void Main(string[] args)
+        {
+            ternaryOperator();
+
+
+        }
+
+        /// <summary>
+        /// this operator same with if else statemenet
+        /// </summary>
+        public static void ternaryOperator()
+        {
+            int val = 70;
+
+            if(val < 50)
+            {
+                Console.WriteLine("the val is small ");
+            }
+            else
+            {
+                Console.WriteLine("the val is large");
+
+            }
+
+
+            //now with ternary operator
+            Console.WriteLine(val < 50 ? "the val is small" : "the val is large" );
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ IntegrationPathToCsharp/IfElseStuff/SwitchstatementsPractice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationPathToCsharp.IfElseStuff
+{
+    /// <summary>
+    /// 
+    /// practice switch case
+    /// </summary>
+    public class SwitchstatementsPractice
+    {
+        static void Main(string[] args)
+        {
+
+            int TheVal = 43;
+            //TODO: TheVal SWITCH STATEMENTS
+
+            switch (TheVal)
+            {
+                case 50:
+                    Console.WriteLine("the val is 50");
+                    break;
+                case 59:
+                    Console.WriteLine("the val is large than 50 "+ TheVal);
+                    break;
+                case 40:
+                case 43:
+                case 42:
+                    Console.WriteLine("the val between 40 - 43  and entered value is: " + TheVal);
+                    break;
+                    default:
+                    Console.WriteLine("the val something else");
+                    break ;
+            }
+        }
+
+    }
+}
