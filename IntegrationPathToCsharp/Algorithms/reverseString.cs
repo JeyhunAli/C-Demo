719cfc8605e42a96c7351e16d7e409380867c711
Jeyhun <jeyhun_aliyev@partech.com>
2022-09-02 10:17:09 -0400

    Added classes and notes

---
+++ IntegrationPathToCsharp/Algorithms/reverseString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationPathToCsharp.Algorithms
+{
+    public class reverseString
+    {
+        static void Main(string[] args)
+        {
+            // ReverseStringFunction_1();
+               ReverseStringFunction_2();
+        }
+
+
+        //this method is reversing only one word 
+        public static void ReverseStringFunction_1()
+        {
+            string s = "programming";
+            Console.WriteLine($"string before reversed  -> {s}");
+            string rev = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                rev = rev + s[i];
+            }
+            Console.WriteLine($"reversed string  -> {rev}");
+        }
+
+        //this method is reversing from user input
+        public static void ReverseStringFunction_2()
+        {
+            string s, revString = "";
+            Console.WriteLine("Plz Enter string: ");
+            s = Console.ReadLine();
+            s = s.ToLower();
+            for(int i = s.Length - 1; i >= 0; i--)
+            {
+                revString = revString + s[i];
+            }
+            Console.WriteLine("Reversed String is : " + revString);
+        }
+    }
+}
---
+++ IntegrationPathToCsharp/Collections/ArrayExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationPathToCsharp.Collections
+{
+    public class ArrayExample
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[4];
+            names[0] = "Jey";
+            names[1] = "Ali";
+            names[2] = "Duncan";
+            names[3] = "Kot";
+            Console.WriteLine(names[0]);
+        }
+
+    }
+}
