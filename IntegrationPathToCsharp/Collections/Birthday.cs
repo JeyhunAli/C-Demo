093de4ba56635b6f9237c6ab68e7c4175bcb9927
Jeyhun <jeyhun_aliyev@partech.com>
2022-11-06 10:12:37 -0500

    update

---
+++ IntegrationPathToCsharp/Collections/Birthday.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationPathToCsharp.Collections
+{
+    internal class Birthday
+    {
+        public static void Main(string[] args)
+        {
+
+            string name = "Artyom Gujin";
+            int age = 26;
+            
+            if(name == "Artyom Gujin" && age == 26)
+            {
+                Console.WriteLine("Happy Brithday Artyom");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("let us know when is your birthday");
+            }
+        }
+
+    }
+}
---
+++ IntegrationPathToCsharp/forEach/cars.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationPathToCsharp.forEach
+{
+    internal class cars
+    {
+        public static void  Main(string[] args)
+        {
+            string[] cars = { "BMW", "FORD", "HYUNDAI" };
+
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+           
+        }
+    }
+}
+
+
+//one
+//downside with a for each loop is that
+//it's not flexible with a standard for
+//loop we can iterate forwards   --   ++   i+=2
+//backwards or even skip iterations so use
+//whatever loop is more appropriate for
+//your situation
